10021f25da0ef728d2c2633c6830066afa64091a
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-16 13:39:34 -0300

    A first style for the login page is created.

--- projeto-modulo1/src/pages/login/index.jsx
+++ projeto-modulo1/src/pages/login/index.jsx
@@ -1,31 +1,37 @@
 import { useForm } from "react-hook-form"
+import { LoginStyled } from './styled'
 
 function LoginPage() {
     const { register, handleSubmit } = useForm()
 
     const onSubmit = (dataFromForm) => {
-        alert('Proceed...')
+        // alert('Proceed...')
+        // alert(dataFromForm.email)
+        // alert(dataFromForm.password)
         localStorage.setItem('user', JSON.stringify(dataFromForm))
     }
 
 
     return (
         <>
-            <h1>Login Page</h1>
-            <form>
-                <input type="email" {...register('email', {
-                    required: true,
-                    pattern: /[A-Za-z]{3}[@a-z]{3}/
-                })} />
-                <input type="password" {...register('password', {
-                    required: true,
-                    pattern: /[a-zA-Z0-9@!#]{8}/
-                })} />
+            <LoginStyled>
+                <h1>Login Page</h1>
+                <form>
+                    <input type="email" {...register('email', {
+                        required: true,
+                        pattern: /[A-Za-z0-9]{3}@*\.[a-z]{2}/,
+                        setValueAs: v => v.trim()
+                    })} />
+                    <input type="password" {...register('password', {
+                        required: true,
+                        pattern: /[a-zA-Z0-9@!#]{8}/
+                    })} />
 
-                <a href="#">Esqueci minha senha?</a>
+                    <a href="#">Esqueci minha senha?</a>
 
-                <button onClick={handleSubmit(onSubmit)} >Entrar</button>
-            </form >
+                    <button onClick={handleSubmit(onSubmit)} >Entrar</button>
+                </form >
+            </LoginStyled>
         </>
     )
 }
---
+++ projeto-modulo1/src/pages/login/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const LoginStyled = styled.main`
+display: flex;
+align-items: center;
+justify-content: center;
+place-content: center;
+`
+
+export { LoginStyled }
