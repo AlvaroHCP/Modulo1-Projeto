0ed10ca2eec5afcc2872c6309998f2ebe9fdbd90
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-15 22:43:44 -0300

    Login inputs created.

--- projeto-modulo1/src/pages/login/index.jsx
+++ projeto-modulo1/src/pages/login/index.jsx
@@ -1,7 +1,31 @@
+import { useForm } from "react-hook-form"
+
 function LoginPage() {
+    const { register, handleSubmit } = useForm()
+
+    const onSubmit = (dataFromForm) => {
+        alert('Proceed...')
+        localStorage.setItem('user', JSON.stringify(dataFromForm))
+    }
+
+
     return (
         <>
             <h1>Login Page</h1>
+            <form>
+                <input type="email" {...register('email', {
+                    required: true,
+                    pattern: /[A-Za-z]{3}[@a-z]{3}/
+                })} />
+                <input type="password" {...register('password', {
+                    required: true,
+                    pattern: /[a-zA-Z0-9@!#]{8}/
+                })} />
+
+                <a href="#">Esqueci minha senha?</a>
+
+                <button onClick={handleSubmit(onSubmit)} >Entrar</button>
+            </form >
         </>
     )
 }
