11553e1759fff2a698656426f50855ec4df68357
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-17 16:17:09 -0300

    Advancing on error types.

--- projeto-modulo1/src/components/inputField/index.jsx
+++ projeto-modulo1/src/components/inputField/index.jsx
@@ -1,24 +1,44 @@
 import { TextField } from "@mui/material";
 
 
-function InputForm({ name, title, type, required, readOnly }) {
+function InputForm({ error, name, title, type, required, readOnly }) {
+
     required != true ? required = false : required = true
     readOnly != true ? readOnly = false : readOnly = true
 
+    let erMessage = 'Error'
+    let incorrectEntry = "Incorrect entry."
+
+    if (error == true) {
+        title = erMessage
+    } else { error = false }
+
     return (
-        <TextField
-            id={name}
-            variant="standard"
-            required={required}
-            label={title}
-            type={type}
-            InputLabelProps={{
-                shrink: true,
-            }}
-            InputProps={{
-                readOnly: readOnly,
-            }}
-        />
+        <>
+            <TextField
+                error={error}
+                id={name}
+                variant="standard"
+                required={required}
+                label={title}
+                type={type}
+                helperText={incorrectEntry}
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                InputProps={{
+                    readOnly: readOnly,
+                }}
+            />
+            <TextField
+                error={true}
+                id="standard-error-helper-text"
+                label="Error"
+                defaultValue="Hello World"
+                helperText="Incorrect entry."
+                variant="standard"
+            />
+        </>
     )
 }
 
