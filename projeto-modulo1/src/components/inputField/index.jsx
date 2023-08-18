ddda159e7a47b909d700b90db4d819fa4eed477d
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-18 13:29:59 -0300

    Starting to make the buttons using the map function.

--- projeto-modulo1/src/components/inputField/index.jsx
+++ projeto-modulo1/src/components/inputField/index.jsx
@@ -1,43 +1,117 @@
+import './styled.js'
 import { TextField } from "@mui/material";
 
 
-function InputForm({ error, name, title, type, required, readOnly }) {
+function InputForm({ error, name, title, type, required, readOnly, storage, errorStorage }) {
 
     required != true ? required = false : required = true
     readOnly != true ? readOnly = false : readOnly = true
 
-    let erMessage = 'Error'
-    let incorrectEntry = "Incorrect entry."
+    // let erMessage = 'Error'
+    let erTitle = title
+    let message
+    let erMessage
+
+
+    switch (type) {
+        case 'email':
+            message = 'seunome@dominio.algo'
+            erMessage = 'seunome@dominio.algo'
+
+            break
+
+        case 'password':
+            message = ''
+            erMessage = ''
+
+            break
+
+        case 'number':
+            message = 'Digite apenas números.'
+            erMessage = 'Digite apenas números.'
+
+            break
+
+        case 'text':
+            message = ''
+            erMessage = ''
+
+            break
+
+
+        default:
+            message = ''
+            erMessage = 'Mensagem fora das regras deste campo.'
+
+            break
+    }
 
     if (error == true) {
-        title = erMessage
+        title = erTitle
     } else { error = false }
+
+    const errorInput = (errorStorage) => {
+        // console.log(Object.keys(errorStorage))       
+        // console.log(errorStorage[title].type)
+        if (errorStorage[title]?.type) {
+            console.log(errorStorage[title].type)
+            return
+        }
+        console.log(errorStorage[title] == undefined)
+    }
 
     return (
         <>
-            <TextField
-                error={error}
-                id={name}
-                variant="standard"
-                required={required}
-                label={title}
-                type={type}
-                helperText={incorrectEntry}
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                InputProps={{
-                    readOnly: readOnly,
-                }}
-            />
-            <TextField
-                error={true}
-                id="standard-error-helper-text"
-                label="Error"
-                defaultValue="Hello World"
-                helperText="Incorrect entry."
-                variant="standard"
-            />
+            {errorStorage[title] == undefined || errorStorage[title].type != 'pattern' ? (
+                <TextField
+                    error={error}
+                    id={name}
+                    variant="standard"
+                    required={required}
+                    label={title}
+                    type={type}
+                    helperText={message}
+                    // onChange={errorInput(errorStorage)}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    InputProps={{
+                        readOnly: readOnly,
+                    }}
+                    {...storage(title, {
+                        pattern: type === 'email' ? /[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]{1,}$/ :
+                            (type === 'password' ? /^[A-Za-z\d@$!%*#?&]{8,}$/ :
+                                (type === 'number' ? /[0-9]/ :
+                                    (type === 'text' ? /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]*$/ : /[a-zA-Z0-9]/))),
+                        setValueAs: v => (v == '' || v == undefined) && required == true ?
+                            errorStorage[title] = 'pattern' :
+                            v
+                    })}
+                />) : (
+                <TextField
+                    error={error = true}
+                    id={name}
+                    variant="standard"
+                    required={required}
+                    label={title}
+                    type={type}
+                    helperText={erMessage}
+                    // onChange={errorInput(errorStorage)}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    InputProps={{
+                        readOnly: readOnly,
+                    }}
+                    {...storage(title, {
+                        pattern: type === 'email' ? /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]{1,}$/ :
+                            (type === 'password' ? /^[A-Za-z\d@$!%*#?&]{8,}$/ :
+                                (type === 'number' ? /[0-9]/ :
+                                    (type === 'text' ? /[a-zA-Z0-9]/ : /[a-zA-Z0-9]/))),
+                    })}
+                />
+            )
+            }
         </>
     )
 }
--- projeto-modulo1/src/pages/drugStoreSubscription/index.jsx
+++ projeto-modulo1/src/pages/drugStoreSubscription/index.jsx
@@ -1,14 +1,96 @@
 import { InputForm } from "../../components/inputField"
+import { TextField } from "@mui/material";
+import { useForm } from "react-hook-form"
+
 
 function DrugStoreSubscription() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+
+    const onSubmit = (dataFromForm) => {
+        console.log(dataFromForm)
+    }
+
+
+    // const currencies = [
+    //     {
+    //         value: 'USD',
+    //         label: '$',
+    //     },
+    //     {
+    //         value: 'EUR',
+    //         label: '€',
+    //     },
+    //     {
+    //         value: 'BTC',
+    //         label: '฿',
+    //     },
+    //     {
+    //         value: 'JPY',
+    //         label: '¥',
+    //     },
+    // ];
+
+
+    const fields = [
+        {
+            name: 'SocialName',
+            title: 'Razão Social',
+            type: 'text',
+            required: true,
+            error: false,
+        },
+        {
+            name: 'Email',
+            title: 'E-mail',
+            type: 'email',
+            required: true,
+            error: false,
+        },
+    ]
+
+
     return (
         <form>
-            <InputForm
-                name={'SocialName'}
-                title={'Razão Social'}
-                type={'text'}
-                required={true}
-            />
+
+            {fields.map(({ name, title, type, required, error }) => {
+                return (
+                    <InputForm
+                        key={name}
+                        error={error}
+                        name={name}
+                        title={title}
+                        type={type}
+                        required={required}
+                        storage={register}
+                        errorStorage={errors}
+                    />
+                )
+            })}
+
+
+
+
+            {/* <TextField
+                id="standard-select-currency-native"
+                select
+                label="Native select"
+                defaultValue="EUR"
+                SelectProps={{
+                    native: true,
+                }}
+                helperText="Please select your currency"
+                variant="standard"
+                >
+                {currencies.map((option) => (
+                    <option key={option.value} value={option.value}>
+                    {option.label}
+                    </option>
+                    ))}
+                </TextField> */}
+
+
+            <button onClick={handleSubmit(onSubmit)}>Vai</button>
         </form>
     )
 }
