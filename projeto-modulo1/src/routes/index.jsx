2a32ff523f0c8c1568e8873c1fa1b59616086138
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-21 19:30:09 -0300

    Created the 404 Page, and the routes for the Header components.

--- projeto-modulo1/src/routes/index.jsx
+++ projeto-modulo1/src/routes/index.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom"
 import { LoginPage } from "../pages/login"
+import { NotFound404 } from '../pages/404'
 import { DrugStoreSubscription } from "../pages/drugStoreSubscription"
 
 function RoutesApp() {
     return (
         <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/DrugSubs" element={<DrugStoreSubscription />} />
+            <Route path="*" element={<NotFound404 />} />
+            <Route path="/DrugsSubs" element={<NotFound404 />} />
+            <Route path="/DrugsList" element={<NotFound404 />} />
+            <Route path="/DrugStoresSubs" element={<DrugStoreSubscription />} />
+            <Route path="/DrugStoresMap" element={<NotFound404 />} />
         </Routes>
     )
 }
