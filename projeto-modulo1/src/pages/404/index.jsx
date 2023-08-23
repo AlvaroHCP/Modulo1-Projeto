cb3f9b694317c522b7caabf2299f6db9f487d274
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-23 15:46:27 -0300

    Creating the folder of the component Footer.

--- projeto-modulo1/src/pages/404/index.jsx
+++ projeto-modulo1/src/pages/404/index.jsx
@@ -11,12 +11,12 @@ function NotFound404() {
         navigate(path)
     }
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         goTo('/')
+    useEffect(() => {
+        setTimeout(() => {
+            goTo('/')
 
-    //     }, timer)
-    // })
+        }, timer)
+    })
 
     const goBack = (event) => {
         event.preventDefault
