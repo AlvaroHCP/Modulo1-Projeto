29dd2ed34cdade171e03c9ced445a276018a2e58
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-28 17:19:56 -0300

    FIX Via cep API.

--- projeto-modulo1/src/pages/drugStoreSubscription/index.jsx
+++ projeto-modulo1/src/pages/drugStoreSubscription/index.jsx
@@ -1,6 +1,6 @@
 import { InputForm } from "../../components/InputField"
 import { useForm } from "react-hook-form"
-import { FindPostalCode } from '../../services/ViaCEPAPI';
+import { FindPostalCode } from '../../services/ViaCepAPI.jsx';
 import { useEffect, useRef } from "react";
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer'
