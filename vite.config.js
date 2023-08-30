76c5c5e20ca0a78b2e60d91957553febf7418932
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-30 18:25:11 -0300

    trying to reload the pages.

--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,5 @@ export default defineConfig({
     rewrites: [
       { source: "/(.*)", destination: "/" }
     ]
-  },
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes('node_modules')) {
-  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  }
 })
