50f3ddb6950d10f1474a56e1f8ebf78cd99a1ac5
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-08-31 11:21:24 -0300

    Styling the 404 page.

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -2,10 +2,11 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { PageNotFound } from './styled'
 import { ButtonArea } from './styled'
+import Button from '@mui/material/Button';
 
 function NotFound404() {
     const navigate = useNavigate()
-    let timer = 3000
+    let timer = 5000
 
     const goTo = (path) => {
         navigate(path)
@@ -27,16 +28,22 @@ function NotFound404() {
         // <h1 style={{ textAlign: "center" }}>Erro 404. Página não encontrada</h1>
         <>
             <PageNotFound>
-                <section>
+                <section style={{ width: '800px' }}>
                     <h1>
                         Erro 404. Página não encontrada!
                     </h1>
-                    <p>
-                        Você está sendo redirecionado para a página principal em 3 segundos!!!
+                    <p style={{ textAlign: 'center' }}>
+                        Você será redirecionado para a <u>Página de Medicamentos</u> em <br /> {timer / 1000} segundos!!!
                     </p>
                     <h3>Ou clique no botão abaixo para voltar à página anterior!</h3>
                     <ButtonArea>
-                        <button onClick={(event) => goBack(event)}>Voltar</button>
+                        {/* <button onClick={(event) => goBack(event)}>Voltar</button> */}
+                        <Button
+                            onClick={(event) => goBack(event)}
+                            autoFocus
+                        >
+                            <b>Página Anterior</b>
+                        </Button>
                     </ButtonArea>
                 </section>
             </PageNotFound>
--- src/pages/404/styled.js
+++ src/pages/404/styled.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
 
 export const PageNotFound = styled.div`
-height: 80vh;
+height: 100vh;
 width: 100%;
+/* background-color: #fbb5cd; */
+/* background-color: #ffd2e7; */
+background-color: #fca3b7;
 display: flex;
 flex-direction: column;
 align-items: center;
@@ -12,29 +15,45 @@ justify-content: center;
 section {
     padding: 30px; 
     padding-Bottom: 60px;
-    border: 4px solid blue; 
+    background-color: white;
+    border: 4px solid pink; 
     border-Radius: 15px;
+    box-shadow: 5;
 }
 
 h1{
     font-size: 40px;
+    margin-bottom: 50px;
 }
 
 p{
-font-size: 22px;
-color: red;
+    align-self: center;
+    font-size: 22px;
+    color: red;
 }
 
 h3{
-    margin-top: 30px;
+    margin-top: 50px;
     margin-Bottom: 25px;
 }
 `
 export const ButtonArea = styled.div`
 
+margin-top: 40px;
 width: 100%;
+display: flex;
+flex-direction: column;
+/* align-self: center; */
+align-items: center;
 
 button{
-    
+    width: 300px;
+    background-color: lightcyan;
+    border: 2px solid lightblue;
+
+    :hover{
+        background-color: lightgreen;
+
+    }
 }
 `
